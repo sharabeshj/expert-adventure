088c032437eae8d9ed663406612d00ec5f743354
sharabesh <sharabeshjayaraman@gmail.com>
2018-04-21 13:37:09 +0530

    update django for whitenoise static

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,4 +130,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR,"static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR,"live-static-files","static-root")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"live-static-files","media-root")
